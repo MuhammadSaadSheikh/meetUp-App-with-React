a0c12e788fc1b8c72684e2864bbbf448c675fc17
saad@usman <msaad9124@gmail.com>
2020-02-20 08:01:19 +0500

    picture upload

--- src/Screens/Profile/index.js
+++ src/Screens/Profile/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 //methods
-import { setUser , uploadPictures } from "../../config/firebase";
+import { setUser, uploadPictures } from "../../config/firebase";
 
 //localStorage
 const userId = localStorage.getItem("userId");
@@ -18,11 +18,12 @@ export default class Profile extends React.Component {
       userImg2: "",
       userImg3: "",
       timeDuration: [],
-      state: "image"
+      stage: "image"
     };
   }
 
   stateHandling = (key, value) => {
+    console.log("imges", value);
     this.setState({
       [key]: value
     });
@@ -37,7 +38,7 @@ export default class Profile extends React.Component {
     }
     try {
       await setUser(userId, { userName, usreNumber });
-      this.setState({ state: "beverage" });
+      this.setState({ stage: "images" });
     } catch (error) {
       console.log("basicError==>", error);
     }
@@ -55,258 +56,271 @@ export default class Profile extends React.Component {
     }
   };
 
-// }
-    userImage = async()=>{
-      console.log('img')
-      const {userImg1, userImg2, userImg3} = this.state
   
-      if(!userImg1 || userImg2 || userImg3){
-        alert("Please upload Pictures!")
-        return
-      }
-      try {
-        let images = [userImg1, userImg2, userImg3]
-        const userProfileImg = await uploadPictures(images)
-  
-        await setUser(userId , {userProfileImg})
-        this.setState({state : 'beverage'})
-      } catch (error) {
-        console.log('images error ==>' , error)
-      }
-  
-      }
+  userImage = async () => {
+    const { userImg1, userImg2, userImg3 } = this.state;
+    if (!userImg1 || !userImg2 || !userImg3) {
+      alert("All fields required!");
+      return;
+    }
+    try {
+      let images = [userImg1, userImg2, userImg3];
+      const userProfileImg = await uploadPictures(images);
+      await setUser(userId, { userProfileImg });
+      this.setState({ stage: "beverage"});
+    }
+     catch (error) {
+      console.log("images error ==>", error);
+    }
+  };
 
   forMeeting = async () => {
-    const { beveragesArr, timeDuration } = this.state
-    const { history } = this.props
+    const { beveragesArr, timeDuration } = this.state;
+    const { history } = this.props;
     try {
-      setUser(userId, { beveragesArr, timeDuration })
-      alert("Meeting set")
-      history.replace('/location')
+      setUser(userId, { beveragesArr, timeDuration });
+      alert("Meeting set");
+      history.replace("/location");
+    } catch (error) {
+      console.log("setMeeting==>", error);
     }
-    catch (error) {
-      console.log("setMeeting==>", error)
+  };
+
+  beverageCheckBox = (value, isSelected) => {
+    let { beveragesArr } = this.state;
+    if (!isSelected) {
+      for (let i = 0; i < beveragesArr.length; i++) {
+        if (beveragesArr[i] == value) {
+          beveragesArr.splice(i, 1);
+        }
+      }
+    } else {
+      beveragesArr.push(value);
+      this.setState({ beveragesArr });
     }
-  }
+    // console.log("beverage", beveragesArr);
+  };
 
-    beverageCheckBox = (value, isSelected) => {
-      let { beveragesArr } = this.state;
-      if (!isSelected) {
-        for (let i = 0; i < beveragesArr.length; i++) {
-          if (beveragesArr[i] == value) {
-            beveragesArr.splice(i, 1);
-          }
+  timeDurationCheckBox = (value, isSelected) => {
+    let { timeDuration } = this.state;
+    if (!isSelected) {
+      for (let i = 0; i < timeDuration.length; i++) {
+        if (timeDuration[i] == value) {
+          timeDuration.splice(i, 1);
         }
       }
-      else {
-        beveragesArr.push(value);
-        this.setState({ beveragesArr });
-      }
-      // console.log("beverage", beveragesArr);
-    };
+    } else {
+      timeDuration.push(value);
+      this.setState({ timeDuration });
+    }
+  };
 
-    timeDurationCheckBox = (value, isSelected) => {
-      let { timeDuration } = this.state;
-      if (!isSelected) {
-        for (let i = 0; i < timeDuration.length; i++) {
-          if (timeDuration[i] == value) {
-            timeDuration.splice(i, 1)
-          }
-        }
+  beverageAndTimeSubmit = async () => {
+    const { history } = this.props;
+    const { beveragesArr, timeDuration, state } = this.state;
+    try {
+      if (beveragesArr.length && timeDuration.length) {
+        setUser(userId, { beveragesArr, timeDuration });
+        history.replace("/location");
+      } else {
+        this.setState({ state: "beverage" });
       }
-      else {
-        timeDuration.push(value)
-        this.setState({ timeDuration })
-      }
-    };
+    } catch (error) {
+      console.log("timeDurationError", error);
+    }
+  };
 
-    beverageAndTimeSubmit = async () => {
-      const { history } = this.props;
-      const { beveragesArr, timeDuration, state } = this.state;
-      try {
-        if (beveragesArr.length && timeDuration.length) {
-          setUser(userId, { beveragesArr, timeDuration });
-          history.replace("/location");
-        } else {
-          this.setState({ state: "beverage" });
-        }
-      } catch (error) {
-        console.log("timeDurationError", error);
-      }
-    };
-
-    render() {
-      const {
-        state,
-        pictureArr,
-        userName,
-        usreNumber,
-        beveragesArr,
-        timeduration
-      } = this.state;
-      return (
-        <div className="mainContainer">
-          {state == "basic" && (
-            <div className="neatedWrapper">
-              <h1>Welcome</h1>
-              <div className="inputWrapper">
-                <input
-                  type="text"
-                  value={userName}
-                  placeholder="Enter your nick Name"
-                  onChange={e =>
-                    this.stateHandling("userName", e.currentTarget.value)
-                  }
-                />
-                <input
-                  type="number"
-                  value={usreNumber}
-                  placeholder="Enter your phone number"
-                  onChange={e =>
-                    this.stateHandling("usreNumber", e.currentTarget.value)
-                  }
-                />
-              </div>
-              <button className="nextButton" onClick={this.forBasic}>
-                Next
-            </button>
+  render() {
+    const {
+      stage,
+      pictureArr,
+      userName,
+      usreNumber,
+      beveragesArr,
+      timeduration
+    } = this.state;
+    return (
+      <div className="mainContainer">
+        {stage == "basic" && (
+          <div className="neatedWrapper">
+            <h1>Welcome</h1>
+            <div className="inputWrapper">
+              <input
+                type="text"
+                value={userName}
+                placeholder="Enter your nick Name"
+                onChange={e =>
+                  this.stateHandling("userName", e.currentTarget.value)
+                }
+              />
+              <input
+                type="number"
+                value={usreNumber}
+                placeholder="Enter your phone number"
+                onChange={e =>
+                  this.stateHandling("usreNumber", e.currentTarget.value)
+                }
+              />
             </div>
-          )}
-          {state == 'image' && (
-            <div className="optionsWrapper">
+            <button className="nextButton" onClick={this.forBasic}>
+              Next
+            </button>
+          </div>
+        )}
+        {stage == "image" && (
+          <div className="optionsWrapper">
             <div className="beverageWrapper">
               <h1>Uplaod Your Picture</h1>
               <div className="nestedchild">
                 <ol>
                   <li>
-                  <input type='file' className='fileField'/>
+                    <input
+                      type="file"
+                      className="fileField"
+                      onChange={e =>
+                        this.stateHandling("userImg1", e.currentTarget.files[0])
+                      }
+                      accept="image/x-png, image/jpg , image/jpeg"
+                    />
                   </li>
                   <li>
-                  <input type='file' className='fileField'/>
+                    <input
+                      type="file"
+                      className="fileField"
+                      onChange={e => {
+                        this.stateHandling("userImg2", e.currentTarget.files[0]);
+                      }}
+                    />
                   </li>
                   <li>
-                  <input type='file' className='fileField'/>
+                    <input
+                      type="file"
+                      className="fileField"
+                      onChange={e =>
+                        this.stateHandling("userImg3", e.currentTarget.files[0])
+                      }
+                    />
                   </li>
                 </ol>
               </div>
               <div className="buttonWrapper">
-                <button className="nextBtn" onClick={this.forBeverages}>
+                <button className="nextBtn" onClick={this.userImage}>
                   NEXT
-              </button>
+                </button>
               </div>
             </div>
           </div>
-          )}
-          {state == "beverage" && (
-            <div className="optionsWrapper">
-              <div className="beverageWrapper">
-                <h1>What do you want on meeting?</h1>
-                <div className="nestedchild">
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="cocktail"
-                      onClick={e => {
-                        this.beverageCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>Cocktail</label>
-                  </div>
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="juice"
-                      onClick={e => {
-                        this.beverageCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>Juice</label>
-                  </div>
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="coffee"
-                      onClick={e => {
-                        this.beverageCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>Coffee</label>
-                  </div>
+        )}
+        {stage == "beverage" && (
+          <div className="optionsWrapper">
+            <div className="beverageWrapper">
+              <h1>What do you want on meeting?</h1>
+              <div className="nestedchild">
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="cocktail"
+                    onClick={e => {
+                      this.beverageCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>Cocktail</label>
                 </div>
-                <div className="buttonWrapper">
-                  <button className="nextBtn" onClick={this.forBeverages}>
-                    NEXT
-                </button>
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="juice"
+                    onClick={e => {
+                      this.beverageCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>Juice</label>
+                </div>
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="coffee"
+                    onClick={e => {
+                      this.beverageCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>Coffee</label>
                 </div>
               </div>
-            </div>
-          )}
-          {state === "meetingTime" && (
-            <div className="optionsWrapper">
-              <div className="beverageWrapper">
-                <h1>Duration of meeting!</h1>
-                <div className="nestedchild">
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="30"
-                      onClick={e => {
-                        this.timeDurationCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>30 mintues</label>
-                  </div>
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="60"
-                      onClick={e => {
-                        this.timeDurationCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>90 mintues</label>
-                  </div>
-                  <div className="checkBoxWrapper">
-                    <input
-                      type="checkbox"
-                      value="120"
-                      onClick={e => {
-                        this.timeDurationCheckBox(
-                          e.currentTarget.value,
-                          e.currentTarget.checked
-                        );
-                      }}
-                    />
-                    <label>120 mintues</label>
-                  </div>
-                </div>
-                <div className="buttonWrapper">
-                  <button
-                    className="nextBtn"
-                    onClick={this.beverageAndTimeSubmit}
-                  >
-                    NEXT
+              <div className="buttonWrapper">
+                <button className="nextBtn" onClick={this.forBeverages}>
+                  NEXT
                 </button>
-                </div>
               </div>
             </div>
-          )}
-        </div>
-      );
-    }
+          </div>
+        )}
+        {stage === "meetingTime" && (
+          <div className="optionsWrapper">
+            <div className="beverageWrapper">
+              <h1>Duration of meeting!</h1>
+              <div className="nestedchild">
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="30"
+                    onClick={e => {
+                      this.timeDurationCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>30 mintues</label>
+                </div>
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="60"
+                    onClick={e => {
+                      this.timeDurationCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>90 mintues</label>
+                </div>
+                <div className="checkBoxWrapper">
+                  <input
+                    type="checkbox"
+                    value="120"
+                    onClick={e => {
+                      this.timeDurationCheckBox(
+                        e.currentTarget.value,
+                        e.currentTarget.checked
+                      );
+                    }}
+                  />
+                  <label>120 mintues</label>
+                </div>
+              </div>
+              <div className="buttonWrapper">
+                <button
+                  className="nextBtn"
+                  onClick={this.beverageAndTimeSubmit}
+                >
+                  NEXT
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
   }
+}
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -26,31 +26,55 @@ export const setUser = (userId, playload) => {
     .set(playload, { merge: true });
 };
 
-export const uploadPictures = files => {
-  const response = Promise.all(
-    files.map(files => {
-      const fileName = Math.floor(Math.random() * 1000000);
-      return new Promise((resolve, rejected) => {
-        storage
-          .ref()
-          .child("/images" + fileName + ".jpg")
-          .put(files)
-          .then(() => {
-            storage
-              .ref()
-              .child("images" + fileName + "jpg")
-              .getDownloadURL()
-              .then(uri => {
-                resolve(uri);
-              });
-          });
-      });
-    })
-  );
-  return response
-};
+// export const uploadPictures = files => {
+//   const response = Promise.all(
+//     files.map(files => {
+//       const fileName = Math.floor(Math.random() * 1000000);
+//       return new Promise((resolve, rejected) => {
+//         storage
+//           .ref()
+//           .child("/images" + fileName + ".jpg")
+//           .put(files)
+//           .then(() => {
+//             storage
+//               .ref()
+//               .child("images" + fileName + "jpg")
+//               .getDownloadURL()
+//               .then(uri => {
+//                 resolve(uri);
+//               });
+//           });
+//       });
+//     })
+//   );
+//   return response
+// };
 
 export const logOut = ()=>{
   localStorage.removeItem('userId')
   return auth.signOut()
+}
+
+
+export const uploadPictures = (files) => {
+  const res = Promise.all(files.map(file => {
+    const fileName = Math.round(Math.random() * 1000000);
+    return new Promise((resolve, reject) => {
+      storage
+        .ref()
+        .child("/images/" + fileName + ".jpg")
+        .put(file).then(() => {
+          storage
+            .ref()
+            .child("/images/" + fileName + ".jpg")
+            .getDownloadURL()
+            .then(uri => {
+              resolve(uri);
+            });
+        })
+    })
+  }))
+  return res.then(result =>{
+    console.log('result==>' , result)
+  })
 }
